8ed8e40d650a55897ff7346f4a3fc2d2fcf2869e
prakai <prakai.na@gmail.com>
2025-09-11 11:12:50 +0700

    Add configuration template

---
+++ include/Configuration-template.h
@@ -0,0 +1,19 @@
+#ifndef _CONFIGURATION_H
+#define _CONFIGURATION_H
+
+#include <Arduino.h>
+
+//
+// WiFi configuration
+//
+String WiFi_ssid = "LOCAL_WIFI_SSID";
+String WiFi_pass = "LOCAL_WIFI_PASSWORD";
+
+//
+// ThingsBoard server and device access token
+//
+String ThingsBoard_server = "iot-so.rmuti.ac.th";
+uint16_t ThingsBoard_port = 1883;
+String ThingsBoard_token = "DEVICE_ACCESS_TOKEN";  // Device Access Token
+
+#endif  // _CONFIGURATION_H
